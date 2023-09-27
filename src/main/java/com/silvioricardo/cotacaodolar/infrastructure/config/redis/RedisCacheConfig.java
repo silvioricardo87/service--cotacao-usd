a96d3651efd1e9b4a0cedd8a0202dbb773f752ca
Silvio Chagas <git@silvioricardo.com>
2023-09-27 19:31:33 -0300

    feat(redis): adicionado configuração de serialização

---
+++ src/main/java/com/silvioricardo/cotacaodolar/infrastructure/config/redis/RedisCacheConfig.java
@@ -0,0 +1,35 @@
+package com.silvioricardo.cotacaodolar.infrastructure.config.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.silvioricardo.cotacaodolar.domain.model.CotacaoDolar;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+  private ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    objectMapper.registerModule(new JavaTimeModule());
+    return objectMapper;
+  }
+
+  @Bean
+  public RedisTemplate<String, CotacaoDolar> redisTemplate(
+      RedisConnectionFactory redisConnectionFactory) {
+
+    RedisTemplate<String, CotacaoDolar> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+    return redisTemplate;
+  }
+}
